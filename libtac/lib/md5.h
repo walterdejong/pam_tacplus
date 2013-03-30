0d385279299361b1fc4fbeadad3a6f35057bdb30
Walter de Jong <walter@heiho.net>
2013-03-30 17:11:30 +0100

    use libssl MD5 functions

--- libtac/lib/md5.h
+++ libtac/lib/md5.h
@@ -1,6 +1,6 @@
-/* md5.h - header file for implementation of MD5
- * 
- * Copyright (C) 1990, RSA Data Security, Inc.
+/* md5.h
+ *
+ * Copyright (C) 2013 Walter de Jong <walter@heiho.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,31 +15,38 @@
  * You should have received a copy of the GNU General Public License
  * along with this program - see the file COPYING.
  *
- * See `CHANGES' file for revision history.
  */
 
-#ifndef __MD5_INCLUDE__
+#ifndef LIBTAC_MD5_H
+#define LIBTAC_MD5_H
 
-#include "libtac.h"
+#ifdef __APPLE__
+  /* use CommonCrypto framework */
 
-/* typedef a 32-bit type */
-typedef unsigned int UINT4;
+  #include <CommonCrypto/CommonDigest.h>
 
-/* Data structure for MD5 (Message-Digest) computation */
-typedef struct {
-    UINT4 i[2];               /* number of _bits_ handled mod 2^64 */
-    UINT4 buf[4];             /* scratch buffer */
-    unsigned char in[64];     /* input buffer */
-    unsigned char digest[16]; /* actual digest after MD5Final call */
-} MD5_CTX;
+  #define MD5_CTX           CC_MD5_CTX
+  #define MD5_Init          CC_MD5_Init
+  #define MD5_Update        CC_MD5_Update
+  #define MD5_Final         CC_MD5_Final
 
-__BEGIN_DECLS
-void MD5Init __P((MD5_CTX*));
-void MD5Update __P((MD5_CTX*, unsigned char*, UINT4));
-void MD5Final __P((unsigned char[], MD5_CTX*));
-__END_DECLS
+  #ifdef MD5_DIGEST_LENGTH
+    #undef MD5_DIGEST_LENGTH
+  #endif
 
-#define MD5_LEN 16
+  #define MD5_DIGEST_LENGTH   CC_MD5_DIGEST_LENGTH
 
-#define __MD5_INCLUDE__
-#endif /* __MD5_INCLUDE__ */
+#else
+  /* use libssl */
+
+  #include <openssl/md5.h>
+
+#endif  /* __APPLE__ */
+
+/* size of a (raw, binary) digest in bytes */
+#define MD5_LEN  16
+
+#endif  /* LIBTAC_MD5_H */
+
+/* EOB */
+
